c3f9558643419a6e216ff67616e7f72fec14eb44
Christy Campbell <christytc10@gmail.com>
2021-02-23 08:31:21 +0000

    Add packet parsing code - still not working

--- GameState.js
+++ GameState.js
@@ -2,25 +2,69 @@ const { F1TelemetryClient, constants } = require('f1-telemetry-client');
 const { PACKETS } = constants;
 
 class GameState {
-    
+
     constructor() {
-        this._standings = ["A. Driver"];
+        this._standings = [];
+        this._participants = new Map();
+        console.log("CONSTRUCTOR");
+        console.log(this._participants);
         this._udpclient = new F1TelemetryClient({ port: 20777, bigintEnabled: true });
         this._udpclient.start();
-        this._udpclient.on(PACKETS.event, console.log);
-        this._udpclient.on(PACKETS.motion, console.log);
-        this._udpclient.on(PACKETS.carSetups, console.log);
-        this._udpclient.on(PACKETS.lapData, console.log);
-        this._udpclient.on(PACKETS.session, console.log);
-        this._udpclient.on(PACKETS.participants, console.log);
-        this._udpclient.on(PACKETS.carTelemetry, console.log);
-        this._udpclient.on(PACKETS.carStatus, console.log);
-        this._udpclient.on(PACKETS.finalClassification, console.log);
-        this._udpclient.on(PACKETS.lobbyInfo, console.log);
+        //this._udpclient.on(PACKETS.event, console.log);
+        //this._udpclient.on(PACKETS.lapData, this.parseLapData);
+        //this._udpclient.on(PACKETS.session, this.parseSessionData);
+        this._udpclient.on(PACKETS.participants, this.parseParticipants);
+        //this._udpclient.on(PACKETS.carStatus, this.parseCarStatusData);
+        //this._udpclient.on(PACKETS.finalClassification, console.log);
+        //this._udpclient.on(PACKETS.lobbyInfo, console.log);
     }
 
     get standings() {
         return this._standings;
+    }
+
+    get participants() {
+        return this._participants;
+    }
+
+    get session_data() {
+        return this._session_data;
+    }
+
+    get car_status() {
+        return this._car_status;
+    }
+
+    set participants(val){
+        this._participants = val;
+    }
+
+    parseParticipants(packet){
+        console.log(this)
+        var i;
+        for (i = 0; i < packet.m_participants.length; i++) {
+            this.participants.set(i, packet.m_participants[i]);
+        }
+    }
+
+    parseLapData(packet){
+        var i;
+        for (i = 0; i < packet.m_lapData.length; i++) {
+            lapData = packet.m_lapData[i];
+            standings_entry = {"driver": participants[i], "lapData": lapData};
+            this._standings[lap_data.m_carPosition] = standings_entry;
+        }
+    }
+
+    parseCarStatusData(packet){
+        var i;
+        for (i = 0; i < packet.m_carStatusData.length; i++) {
+            this._car_status[i] = packet.m_carStatusData[i];
+        }
+    }
+
+    parseSessionData(packet){
+        this._session_data = packet.m_sessionData;
     }
 }
 module.exports = GameState;
--- app.js
+++ app.js
@@ -14,5 +14,4 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
 
     console.log('Example app listening');
-    console.log(gameState.standings)
 });
